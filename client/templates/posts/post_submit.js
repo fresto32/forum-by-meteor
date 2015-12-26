db26d3db62a3f88cdf85e2fc25f0f3cbf0797f35
fresto32 <tajpereira@gmail.com>
2015-12-26 19:20:22 +1000

    Enforced unique URL submissions

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -8,11 +8,15 @@ Template.postSubmit.events({
     };
 
     Meteor.call('postInsert', post, function(error, result) {
-      // display the error to the user and abort
-      if (error)
-        return alert(error.reason);
+          // display the error to the user and abort
+          if (error)
+            return alert(error.reason);
 
-      Router.go('postPage', {_id: result._id});
-    });
-  }
+          // show this result but route anyway
+          if (result.postExists)
+            alert('This link has already been posted');
+
+          Router.go('postPage', {_id: result._id});
+        });
+      }
 });
