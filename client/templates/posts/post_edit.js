edbd6aac2c8d34a8cf21103c565d01318ef55343
fresto32 <tajpereira@gmail.com>
2015-12-28 20:29:08 +1000

    Enforced URL uniqueness for post editting

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -9,14 +9,27 @@ Template.postEdit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
-    Posts.update(currentPostId, {$set: postProperties}, function(error) {
+    Meteor.call('postEdit', postProperties, currentPostId, function(error, result){
+        //display the error and report it
+        if(error)
+          return alert(error.reason);
+
+        //route to the identical URL post
+        if(result.postExists)
+          alert('This link has already been posted');
+
+        Router.go('postPage', {_id: result._id});
+
+    });
+
+/*    Posts.update(currentPostId, {$set: postProperties}, function(error) {
       if (error) {
         // display the error to the user
         alert(error.reason);
       } else {
         Router.go('postPage', {_id: currentPostId});
       }
-    });
+    });*/
   },
 
   'click .delete': function(e) {
