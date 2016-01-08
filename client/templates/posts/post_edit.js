ed9b2caba5740970b76a63c4fcce6f55f3a8dcd1
fresto32 <tajpereira@gmail.com>
2016-01-08 10:50:28 +1030

    Added routes for post lists and pages to display them

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -50,13 +50,13 @@ Template.postEdit.events({
       }
     });*/
 
-  'click .delete': function(e) {
-    e.preventDefault();
+    'click .delete': function(e) {
+        e.preventDefault();
 
-    if (confirm("Delete this post?")) {
-      var currentPostId = this._id;
-      Posts.remove(currentPostId);
-      Router.go('postsList');
-    }
-  }
+        if (confirm("Delete this post?")) {
+          var currentPostId = this._id;
+          Posts.remove(currentPostId);
+          Router.go('home');
+        }
+      }
 });
